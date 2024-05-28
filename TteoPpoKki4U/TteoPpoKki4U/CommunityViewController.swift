262ae31f30c51c4056e58760c339ee9d1f7b5382
Haroldfromk <dongik369@naver.com>
2024-05-28 17:07:47 +0900

    [Feat] Init Project

---
+++ TteoPpoKki4U/TteoPpoKki4U/CommunityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CommunityViewController.swift
+//  TteoPpoKki4U
+//
+//  Created by Dongik Song on 5/28/24.
+//
+
+import UIKit
+
+class CommunityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
