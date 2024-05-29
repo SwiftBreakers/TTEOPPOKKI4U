969704b3a52176d87f24ff76c40e55cb464357c8
moremirim <moremirim@gmail.com>
2024-05-30 00:09:53 +0900

    [Feat] Add UIColor Extension and Theme Color

---
+++ TteoPpoKki4U/TteoPpoKki4U/Model/MyPage/MyPageSection.swift
@@ -0,0 +1,14 @@
+//
+//  MyPageSection.swift
+//  TteoPpoKki4U
+//
+//  Created by 박미림 on 5/29/24.
+//
+
+import Foundation
+
+struct MyPageSection {
+    let title: String
+    let options: [MyPageModel]
+}
+
