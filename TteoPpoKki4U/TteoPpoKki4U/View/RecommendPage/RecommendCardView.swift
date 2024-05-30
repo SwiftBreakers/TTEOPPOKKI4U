115a8bab5b20d08526b8e7fee14cda02912fb36e
jinmoon23 <wlsansdlek1@me.com>
2024-05-30 16:57:27 +0900

    [Feat] 추천페이지 VerticalCardSwiper SPM 동작 확인

---
+++ TteoPpoKki4U/TteoPpoKki4U/View/RecommendPage/RecommendCardView.swift
@@ -0,0 +1,47 @@
+//
+//  RecommendCardView.swift
+//  TteoPpoKki4U
+//
+//  Created by 최진문 on 2024/05/30.
+//
+
+import UIKit
+import VerticalCardSwiper
+
+public class MyCardCell: CardCell {
+    
+    public let titleLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setCardUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            descriptionLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10)
+        ])
+    }
+    public func setCardUI() {
+        let randomRed: CGFloat = .random(in: 0...1)
+        let randomGreen: CGFloat = .random(in: 0...1)
+        let randomBlue: CGFloat = .random(in: 0...1)
+        self.backgroundColor = UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        self.layer.cornerRadius = 12
+    }
+}
