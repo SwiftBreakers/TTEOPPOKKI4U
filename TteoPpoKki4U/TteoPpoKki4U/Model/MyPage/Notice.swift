f586e559c705103b4cdcd7547fd7b81f0b1c1ca7
moremirim <moremirim@gmail.com>
2024-06-25 23:49:51 +0900

    [Feat] NoticeTableViewController, NoticeTableViewCell

---
+++ TteoPpoKki4U/TteoPpoKki4U/Model/MyPage/Notice.swift
@@ -0,0 +1,14 @@
+//
+//  Notice.swift
+//  TteoPpoKki4U
+//
+//  Created by 박미림 on 6/24/24.
+//
+
+import Foundation
+
+struct Notice {
+    let title: String
+    let date: String
+    let detail: String
+}
