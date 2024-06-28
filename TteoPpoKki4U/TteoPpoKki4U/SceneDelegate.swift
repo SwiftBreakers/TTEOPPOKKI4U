25d0b85a4e590c0a3cf09707a923af590cdd138b
Haroldfromk <dongik369@naver.com>
2024-06-28 10:45:56 +0900

    [Feat & Fix] 런치스크린 딜레이추가,  마이페이지 랭크 오류 수정

--- TteoPpoKki4U/TteoPpoKki4U/SceneDelegate.swift
+++ TteoPpoKki4U/TteoPpoKki4U/SceneDelegate.swift
@@ -26,7 +26,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-    
+        
+        Thread.sleep(forTimeInterval: 0.5)
+        
         let window = UIWindow(windowScene: windowScene)
         self.window = window
         
