99ee1a31bd7354726acdd3fa1a219672ab5263a7
thumbsUp-KGE <rjsdmd2172@gmail.com>
2024-06-28 19:35:48 +0900

    [fix]이벤트배너를 마이페이지로 이동

--- TteoPpoKki4U/TteoPpoKki4U/View/MyPage/MyPageViewController.swift
+++ TteoPpoKki4U/TteoPpoKki4U/View/MyPage/MyPageViewController.swift
@@ -13,6 +13,16 @@ import Kingfisher
 
 class MyPageViewController: UIViewController {
     
+    let eventContainerView = UIView()
+    let eventContainerSubView = UIView()
+    let eventContainerSubImageView = UIView()
+    let closeButton = UIButton(type: .system)
+    let doNotShowTodayButton = UIButton(type: .system)
+    let eventImageView = UIImageView()
+    let titleLabel = UILabel()
+    
+    let subTitleLabel = UILabel()
+    
     lazy var myPageView: MyPageView = {
         let view = MyPageView()
         view.editTapped = editTapped
@@ -61,6 +71,9 @@ class MyPageViewController: UIViewController {
         myPageView.collectionView.register(SeparatorView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SeparatorView.identifier)
         
         bind()
+        
+        //아래꺼 추가-이벤트오버레이
+        setupEventOverlay()
     }
     
     
@@ -250,5 +263,170 @@ extension UINavigationController {
         } else {
             completion?()
         }
+    }
+}
+
+
+extension MyPageViewController {
+    
+   
+   
+    
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 저장된 날짜와 현재 날짜 비교
+        if shouldShowEvent() {
+            showEventOverlay()
+        }
+    }
+    
+    func setupEventOverlay() {
+        eventContainerView.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        eventContainerView.isHidden = true
+        view.addSubview(eventContainerView)
+        eventContainerView.addSubview(eventContainerSubView)
+        eventContainerSubView.backgroundColor = UIColor.white
+        eventContainerSubView.layer.cornerRadius = 4
+        eventContainerSubView.addSubview(eventContainerSubImageView)
+        eventContainerView.addSubview(titleLabel)
+        eventContainerView.addSubview(subTitleLabel)
+        
+        eventContainerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        eventContainerSubView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+//            make.top.equalToSuperview().offset(200)
+//            make.leading.equalToSuperview().offset(50)
+            make.height.equalTo(300)
+            make.width.equalTo(250)
+        }
+        
+        eventContainerSubImageView.clipsToBounds = true
+        eventContainerSubImageView.snp.makeConstraints { make in
+            make.top.equalTo(eventContainerSubView.snp.top).offset(15)
+            make.centerX.equalTo(eventContainerSubView)
+//            make.top.equalToSuperview().offset(200)
+//            make.leading.equalToSuperview().offset(50)
+            make.height.equalTo(220)
+            make.width.equalTo(220)
+        }
+        
+        
+        let eventImageView = UIImageView(image: UIImage(named: "sample"))
+        eventImageView.contentMode = .scaleAspectFill
+        eventImageView.clipsToBounds = true
+        eventContainerSubImageView.addSubview(eventImageView)
+        
+        eventImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(eventImageTapped))
+                eventImageView.isUserInteractionEnabled = true
+                eventImageView.addGestureRecognizer(tapGesture)
+        
+        titleLabel.text("리뷰 쓰고 커피 받아가세요!")
+        titleLabel.font = ThemeFont.fontMedium(size: 14)
+
+        titleLabel.textColor = UIColor(hexString: "353535")
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(eventContainerSubImageView.snp.bottom).offset(10)
+//            make.leading.equalTo(eventContainerSubImageView.snp.leading).offset(10)
+//            make.trailing.equalTo(eventContainerSubImageView.snp.trailing).offset(10)
+            make.centerX.equalTo(eventContainerSubView)
+        }
+        
+        subTitleLabel.text("기간 종료 후 20명 순차지급")
+        subTitleLabel.font = ThemeFont.fontRegular(size: 12)
+
+        subTitleLabel.textColor = UIColor(hexString: "353535")
+        subTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(6)
+//            make.leading.equalTo(titleLabel.snp.leading)
+            make.centerX.equalTo(titleLabel)
+        }
+        
+        closeButton.setTitle("닫기", for: .normal)
+        closeButton.titleLabel?.font = ThemeFont.fontMedium(size: 14)
+        closeButton.setTitleColor(UIColor(hexString: "FFFFFF"), for: .normal)
+        closeButton.backgroundColor = UIColor(hexString: "FE724C")
+        closeButton.layer.cornerRadius = 4
+        closeButton.addTarget(self, action: #selector(hideEventOverlay), for: .touchUpInside)
+        eventContainerView.addSubview(closeButton)
+        
+        closeButton.snp.makeConstraints { make in
+            make.trailing.equalTo(eventContainerSubView.snp.trailing)
+//            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-20)
+            make.top.equalTo(eventContainerSubView.snp.bottom).offset(10)
+            make.width.equalTo(60)
+            make.height.equalTo(40)
+        }
+        
+        doNotShowTodayButton.setTitle("오늘 하루 보지 않기", for: .normal)
+        doNotShowTodayButton.titleLabel?.font = ThemeFont.fontRegular(size: 14)
+        doNotShowTodayButton.setTitleColor(UIColor(hexString: "353535"), for: .normal)
+        doNotShowTodayButton.backgroundColor = .white
+        doNotShowTodayButton.layer.cornerRadius = 4
+        doNotShowTodayButton.addTarget(self, action: #selector(doNotShowTodayButtonTapped), for: .touchUpInside)
+        eventContainerView.addSubview(doNotShowTodayButton)
+        
+        doNotShowTodayButton.snp.makeConstraints { make in
+            make.centerY.equalTo(closeButton.snp.centerY)
+            make.leading.equalTo(eventContainerSubView.snp.leading)
+            make.width.equalTo(180)
+            make.height.equalTo(40)
+        }
+    }
+    
+    @objc func eventImageTapped() {
+        if let tabBarController = self.tabBarController {
+                    tabBarController.selectedIndex = 3 // MyPageViewController가 탭바의 네번째에 있음
+                    
+                    if let myPageNavController = tabBarController.viewControllers?[3] as? UINavigationController,
+                       let myPageVC = myPageNavController.viewControllers.first as? MyPageViewController {
+                        myPageVC.showEventSceneViewController()
+                        
+                        
+                    }
+                }
+    }
+    
+    func showEventOverlay() {
+        eventContainerView.isHidden = false
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    @objc func hideEventOverlay() {
+        eventContainerView.isHidden = true
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    @objc func doNotShowTodayButtonTapped() {
+        let currentDate = Date()
+        let userID = Auth.auth().currentUser?.uid ?? "guest" // Firebase Authentication을 사용하여 현재 사용자의 ID를 가져옵니다.
+                    UserDefaults.standard.set(currentDate, forKey: "DoNotShowEventDate_\(userID)") // 사용자별로 날짜를 저장합니다.
+                
+        hideEventOverlay()
+    }
+    
+    func shouldShowEvent() -> Bool {
+        let calendar = Calendar.current
+        let currentDate = Date()
+//        if let savedDate = UserDefaults.standard.object(forKey: "DoNotShowEventDate") as? Date {
+//            if calendar.isDate(currentDate, inSameDayAs: savedDate) {
+//                return false
+//            }
+//        }
+        let userID = Auth.auth().currentUser?.uid ?? "guest" // Firebase Authentication을 사용하여 현재 사용자의 ID를 가져오기
+                  if let savedDate = UserDefaults.standard.object(forKey: "DoNotShowEventDate_\(userID)") as? Date { // 사용자별로 날짜를 불러오기
+                    if calendar.isDate(currentDate, inSameDayAs: savedDate) {
+                        return false
+                    }
+                }
+        
+        return true
     }
 }
